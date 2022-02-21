3fe8fd5be96c0faf0366b495cf2dcd92de2cb198
kimtaeil <xodlf5859@naver.com>
2022-02-21 15:45:03 +0900

    test

---
+++ src/test/java/com/innovationT/InnovationTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovationT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
